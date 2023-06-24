c1267d5c57fe76a900661f0ae58b8ef43f31b183
Felipe <felipe.raindo@gmail.com>
2023-06-24 04:34:10 -0300

    modify category migration to accept defaults table parameters

--- src/migrations/20230624065829-create-category.js
+++ src/migrations/20230624065829-create-category.js
@@ -9,14 +9,17 @@ module.exports = {
       },
       name: {
         type: Sequelize.STRING,
+        allowNull: false,
       },
       created_at: {
         allowNull: false,
         type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
       },
       updated_at: {
         allowNull: false,
         type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'),
       },
     });
   },
