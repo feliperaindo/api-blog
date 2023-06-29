bcdc4e2c745d447dcb936a310d181744bc412eb9
Felipe <felipe.raindo@gmail.com>
2023-06-29 04:25:21 -0300

    implementa novas rotas no arquivo app

--- src/app.js
+++ src/app.js
@@ -5,16 +5,22 @@ const routerManager = require('./routes/exporter');
 
 // Fonte da verdade
 const { routes } = require('./SSOT/exporter');
-// ...
 
+// App e configuração
 const app = express();
-
-// não remova ou mova esse endpoint
-app.get(routes.ROOT, routerManager.ROOT);
-
 app.use(express.json());
 
-// ...
+// não remova ou mova esse endpoint
+app.get(routes.ROOT, routerManager.root);
+
+// Roteamento
+app.use(routes.LOGIN, routerManager.login);
+
+app.use(routes.USER, routerManager.user);
+
+app.use(routes.CATEGORY, routerManager.category);
+
+app.use(routes.POST, routerManager.post);
 
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
