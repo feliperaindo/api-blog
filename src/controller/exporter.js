43fe6b9448fb43dd606a4090f3325b8359cef978
Felipe <felipe.raindo@gmail.com>
2023-06-29 04:26:17 -0300

    atualiza arquivos de exportação/barrel

---
+++ src/controller/exporter.js
@@ -0,0 +1,6 @@
+const post = require('./post/post');
+const user = require('./user/user');
+const login = require('./login/login');
+const category = require('./category/category');
+
+module.exports = { login, user, category, post };
--- src/middleware/exporter.js
+++ src/middleware/exporter.js
@@ -1,3 +1,8 @@
+const postMid = require('./post/postMid');
+const userMid = require('./user/userMid');
 const errorMid = require('./global/errorMid');
+const tokenMid = require('./global/tokenMid');
+const loginMid = require('./login/loginMid');
+const categoryMid = require('./category/categoryMid');
 
-module.exports = { errorMid };
+module.exports = { errorMid, loginMid, userMid, categoryMid, tokenMid, postMid };
