b8e5670adaee3e7f8744a1346142a0a05817cb18
Felipe <felipe.raindo@gmail.com>
2023-06-29 15:50:18 -0300

    create status code file and error messages file

---
+++ src/SSOT/error.js
@@ -0,0 +1,29 @@
+const INVALID_EMAIL = '"email" must be a valid email';
+const INVALID_TOKEN = 'Expired or invalid token';
+const NAME_NOT_FOUND = '"name" is required';
+const POST_NOT_FOUND = 'Post does not exist';
+const MISSING_FIELDS = 'Some required fields are missing';
+const INVALID_FIELDS = 'Invalid fields';
+const USER_NOT_FOUND = 'User does not exist';
+const TOKEN_NOT_FOUND = 'Token not found';
+const INVALID_DISPLAY = '"displayName" length must be at least 8 characters long';
+const USER_REGISTERED = 'User already registered';
+const INVALID_PASSWORD = '"password" length must be at least 6 characters long';
+const USER_UNAUTHORIZED = 'Unauthorized user';
+const CATEGORY_NOT_FOUND = 'one or more "categoryIds" not found';
+
+module.exports = {
+  INVALID_EMAIL,
+  INVALID_TOKEN,
+  NAME_NOT_FOUND,
+  POST_NOT_FOUND,
+  MISSING_FIELDS,
+  INVALID_FIELDS,
+  USER_NOT_FOUND,
+  TOKEN_NOT_FOUND,
+  INVALID_DISPLAY,
+  USER_REGISTERED,
+  INVALID_PASSWORD,
+  USER_UNAUTHORIZED,
+  CATEGORY_NOT_FOUND,
+};
---
+++ src/SSOT/http.js
@@ -0,0 +1,18 @@
+// HTTP status codes
+const OK = 200;
+const CREATED = 201;
+const CONFLICT = 409;
+const NOT_FOUND = 404;
+const NO_CONTENT = 204;
+const BAD_REQUEST = 400;
+const UNAUTHORIZED = 401;
+
+module.exports = {
+  OK,
+  CREATED,
+  CONFLICT,
+  NOT_FOUND,
+  NO_CONTENT,
+  BAD_REQUEST,
+  UNAUTHORIZED,
+};
