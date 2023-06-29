966f1c69668f1eb7074c72800669557574a1f96b
Felipe <felipe.raindo@gmail.com>
2023-06-29 04:24:42 -0300

    cria novas rotas

--- src/SSOT/routes.js
+++ src/SSOT/routes.js
@@ -1,3 +1,19 @@
+const ID = '/:id';
+const ME = '/me';
+const POST = '/post';
 const ROOT = '/';
+const USER = '/user';
+const LOGIN = '/login';
+const CATEGORY = '/categories';
+const SEARCH_TERM = '/search';
 
-module.exports = { ROOT };
+module.exports = { 
+  ID,
+  ME,
+  POST,
+  ROOT,
+  USER,
+  LOGIN,
+  CATEGORY,
+  SEARCH_TERM,
+};
--- src/routes/exporter.js
+++ src/routes/exporter.js
@@ -1,3 +1,7 @@
-const ROOT = require('./root/root');
+const root = require('./root/root');
+const post = require('./post/post');
+const user = require('./user/user');
+const login = require('./login/login');
+const category = require('./category/category');
 
-module.exports = { ROOT };
+module.exports = { root, login, user, category, post };
