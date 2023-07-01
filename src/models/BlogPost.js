6abd8166a542136072972fdbb25a8ca6b80259e7
Felipe <felipe.raindo@gmail.com>
2023-06-30 22:25:20 -0300

    ajust Model BlogPost to insert id and userId fields

--- src/models/BlogPost.js
+++ src/models/BlogPost.js
@@ -1,7 +1,9 @@
 module.exports = (sequelize, DataTypes) => {
   const BlogPost = sequelize.define('BlogPost', {
+    id: { type: DataTypes.INTEGER, primaryKey: true },
     title: DataTypes.STRING,
     content: DataTypes.STRING,
+    userId: DataTypes.INTEGER,
     published: DataTypes.DATE,
     updated: DataTypes.DATE,
   }, { underscored: true, timestamps: false });
